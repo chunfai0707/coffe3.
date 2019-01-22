a6bef7f40febf1ee7a51a2fb0408a431ae809711
chunfai <chunfai0707@gmail.com>
2019-01-22 16:13:04 +0800

    Initial commit

---
+++ src/main/java/com/example/mycoffeeshop/MyCoffeeShopApplication.java
@@ -0,0 +1,14 @@
+package com.example.mycoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoffeeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCoffeeShopApplication.class, args);
+    }
+
+}
+
