8e6d396239a5e9a14e172dc05d01e3cc789b4c2a
chunfai <chunfai0707@gmail.com>
2019-01-22 23:13:08 +0800

    Added data source configuration properties. Added index.html to template. Added testing at MainController.

--- src/main/java/com/example/mycoffeeshop/controller/MainController.java
+++ src/main/java/com/example/mycoffeeshop/controller/MainController.java
@@ -1,8 +1,28 @@
 package com.example.mycoffeeshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @GetMapping(value = "/home")
+    public String hello(ModelMap model) {
+        List<String> list = new ArrayList<>();
+        list.add("Mocha");
+        list.add("Espresso");
+        list.add("Frappucino");
+        list.add("Coffee");
+        list.add("Latte");
+        list.add("Hojicha");
+        list.add("Matcha");
+        list.add("Sakura");
+
+        model.addAttribute("coffee", list);
+        return "index";
+    }
 }
