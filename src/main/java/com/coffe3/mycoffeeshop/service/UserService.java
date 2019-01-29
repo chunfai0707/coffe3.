8c451b92c059f1a4a39c6c015cd2d3c2601734ac
chunfai <chunfai0707@gmail.com>
2019-01-29 16:21:14 +0800

    Added password length validation. Added add role when registering users.

--- src/main/java/com/coffe3/mycoffeeshop/service/UserService.java
+++ src/main/java/com/coffe3/mycoffeeshop/service/UserService.java
@@ -1,6 +1,8 @@
 package com.coffe3.mycoffeeshop.service;
 
+import com.coffe3.mycoffeeshop.domain.Role;
 import com.coffe3.mycoffeeshop.domain.User;
+import com.coffe3.mycoffeeshop.repository.RoleRepository;
 import com.coffe3.mycoffeeshop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -20,6 +22,7 @@ public class UserService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public List<User> getUserByEmail(String email) {
@@ -32,30 +35,40 @@ public class UserService {
         List<String> messages = new ArrayList<>();
 
         if (str[0].equals(str[1])) {
+            if (str[0].length() >= 8) {
+                if (userRepository.getUserByEmail(user.getUserEmail()).isEmpty()) {
 
-            if (userRepository.getUserByEmail(user.getUserEmail()).isEmpty()) {
+                    User userToRegister = new User();
+                    List<Role> roles = new ArrayList<>();
 
-                User userToRegister = new User();
+                    roles.add(roleRepository.findByRoleName("user"));
 
-                userToRegister.setUserName(user.getUserName());
-                userToRegister.setUserEmail(user.getUserEmail());
-                userToRegister.setUserPassword(bCryptPasswordEncoder.encode(str[0]));
-                userToRegister.setUserEnabled(1);
-                userToRegister.setUserCreatedAt(new Date());
-                userToRegister.setUserLastUpdated(new Date());
+                    userToRegister.setUserName(user.getUserName());
+                    userToRegister.setUserEmail(user.getUserEmail());
+                    userToRegister.setUserPassword(bCryptPasswordEncoder.encode(str[0]));
+                    userToRegister.setUserEnabled(1);
+                    userToRegister.setUserCreatedAt(new Date());
+                    userToRegister.setUserLastUpdated(new Date());
+                    userToRegister.setRoles(roles);
 
-                userRepository.saveAndFlush(userToRegister);
-                messages.add("Success");
+                    userRepository.saveAndFlush(userToRegister);
+                    messages.add("Success");
 
-                logger.info("User {} registered.", userToRegister.getUserEmail());
+                    logger.info("User {} registered.", userToRegister.getUserEmail());
 
-                return messages;
+                    return messages;
 
+                } else {
+                    messages.add("Failed");
+                    messages.add("Email Already Registered");
+                    return messages;
+                }
             } else {
                 messages.add("Failed");
-                messages.add("Email Already Registered");
+                messages.add("Password Too Short");
                 return messages;
             }
+
         } else {
             messages.add("Failed");
             messages.add("Passwords Not Match");
